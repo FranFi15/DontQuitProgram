7bd95e0a0ea791676266ff24b5b5f4abf7c48d0a
FranFi15 <102774445+FranFi15@users.noreply.github.com>
2026-02-27 19:51:23 -0300

    ruta

--- server/routes/payment.routes.js
+++ server/routes/payment.routes.js
@@ -1,24 +1,31 @@
 import { Router } from 'express';
-import { reportTransfer, getPendingPayments, approvePayment, rejectPayment, getAdminBadges } from '../controllers/payment.controller.js';
+import { 
+  reportTransfer, 
+  getPendingPayments, 
+  approvePayment, 
+  rejectPayment, 
+  getAdminBadges,
+  getPaymentHistory // 
+} from '../controllers/payment.controller.js';
 import { createPreference, receiveWebhook } from '../controllers/mercadopago.controller.js';
 import { createOrder, captureOrder } from '../controllers/paypal.controller.js';
 
 const router = Router();
 
-// Rutas para los pagos
-router.post('/transfer', reportTransfer); // El cliente reporta el pago
-router.get('/pending', getPendingPayments); //Admin ve los pagos pendientes
-
-router.put('/:id/approve', approvePayment); // Admin aprueba el pago, se crea la suscripción y se notifica al cliente
-router.put('/:id/reject', rejectPayment); // Admin rechaza el pago
-
+// Rutas de consulta (Ponlas arriba)
+router.get('/pending', getPendingPayments); 
+router.get('/history', getPaymentHistory);
 router.get('/badges', getAdminBadges);
 
+// Rutas de acción
+router.post('/transfer', reportTransfer); 
+router.put('/:id/approve', approvePayment); 
+router.put('/:id/reject', rejectPayment); 
+
+// Pasarelas
 router.post('/mp/create_preference', createPreference);
 router.post('/mp/webhook', receiveWebhook);
-
 router.post('/paypal/create-order', createOrder);
 router.post('/paypal/capture-order', captureOrder);
-
 
 export default router;
