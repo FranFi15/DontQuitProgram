7acb87b1cc74fb80bb77ec4207cff94e360b58c0
FranFi15 <102774445+FranFi15@users.noreply.github.com>
2026-03-14 18:50:28 -0300

    dale porfa

--- server/routes/chat.routes.js
+++ server/routes/chat.routes.js
@@ -1,10 +1,10 @@
 import { Router } from 'express';
 import { sendMessage, getConversation, markAsRead, getChatUsers} from '../controllers/chat.controller.js';
-import { verifyToken, isAdmin } from '../middlewares/auth.middleware.js';
+
 
 const router = Router();
 
-router.get('/users', verifyToken, isAdmin, getChatUsers );
+router.get('/users', getChatUsers );
 router.post('/', sendMessage);
 router.get('/:userId1/:userId2', getConversation);
 router.put('/read', markAsRead);
