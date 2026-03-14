c143af00727878175cc6c6dad3b0906364661b1d
FranFi15 <102774445+FranFi15@users.noreply.github.com>
2026-03-14 18:20:15 -0300

    veri

--- server/routes/users.routes.js
+++ server/routes/users.routes.js
@@ -1,7 +1,6 @@
 // server/routes/users.js
 import express from 'express';
 import { getAllUsers, createUser, updateUser, deleteUser, changePassword, getUserSubscriptions, getUserRMs } from '../controllers/user.controller.js';
-import { verifyToken } from '../middlewares/auth.middleware.js';
 import { verifyToken, isAdmin } from '../middlewares/auth.middleware.js';
 
 const router = express.Router();
